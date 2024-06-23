7494f9e14f2f1ed5b9d2fa181b5cd87aaabf9345
Rizvi Fazi <rizvifazi124@gmail.com>
2024-06-23 22:13:06 +0530

    First commit

---
+++ src/test/java/com/pack/SpringInterceptors/SpringInterceptorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.SpringInterceptors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInterceptorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
